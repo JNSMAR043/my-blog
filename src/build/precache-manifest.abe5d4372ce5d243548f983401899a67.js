a6e5173f60f796142bf407756a1d33f9623f51d6
Mark <mark.jones@ectelect.co.za>
2020-07-13 13:09:50 +0200

    Blog demo

---
+++ src/build/precache-manifest.abe5d4372ce5d243548f983401899a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81a8dd1af2bdfe2ab3965593b8e1967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b074ce72d0fa974faf7",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "329f735aab5f46b78de2",
+    "url": "/static/js/2.c391e6de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c391e6de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b074ce72d0fa974faf7",
+    "url": "/static/js/main.f9cbd3b9.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
